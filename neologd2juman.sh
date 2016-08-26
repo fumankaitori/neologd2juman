d0a04b027288334fb905c5561ac6e6cbf8cae320
Mitsuzawa-Kensuke <kensuke.mit@gmail.com>
2016-08-26 16:28:25 +0900

    added unix distribution flag

--- neologd2juman.sh
+++ neologd2juman.sh
@@ -3,8 +3,20 @@
 # 今いるディレクトリ内に、dicファイル、intファイル、jumandic.dat、jumandic.patを生成する
 # optionで出力ディレクトリを指定しようと思ったが、makepatは、実行した場所にjumandic.datがないとこけるので断念
 
+
 binary_path=`dirname $0`
 input_base=`basename $1`
+
+if [ `uname` = "Darwin" ]; then
+    #mac用のコード
+    echo "Use default juman path in mac"
+    juman_utils_bin="/usr/local/opt/juman/libexec/juman/"
+elif [ `uname` = "Linux" ]; then
+    #Linux用のコード
+    echo "Use default juman path in linux"
+    juman_utils_bin="/usr/local/libexec/juman/"
+fi
+
 
 echo 'Start...'
 
@@ -15,7 +27,7 @@ python ${binary_path}/codes/neologd2juman.py < $1 > ./${input_base}.dic
 echo 'End Converting mecab-neologd-ipadic into juman format.'
 
 # jumanで利用できる辞書（jumandic.dat、jumandic.pat）にコンパイル
-/usr/local/opt/juman/libexec/juman/makeint ./${input_base}.dic
+${juman_utils_bin}"makeint" ./${input_base}.dic
 
 # メモリが溢れないようにintファイルを分割
 split -l 500000 ./${input_base}.int ./${input_base}.int-
@@ -23,9 +35,9 @@ split -l 500000 ./${input_base}.int ./${input_base}.int-
 : > jumandic.dat
 for int_file in ./${input_base}.int-*
 do
-  /usr/local/opt/juman/libexec/juman/dicsort ${int_file} >> jumandic.dat
+  ${juman_utils_bin}"dicsort" ${int_file} >> jumandic.dat
 done
 
-/usr/local/opt/juman/libexec/juman/makepat ./${input_base}.int
+${juman_utils_bin}"makepat" ./${input_base}.int
 
 echo 'End Compiling.'
